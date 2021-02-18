d1e2bee253e46009190557a4f0c16988d7bbd089
jiovanie <jiovaniemartinez@gmail.com>
2021-02-17 22:45:22 -0600

    added the slides and navbar

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Jtron from './Components/MainBody/Jtron';
+import Slides from './Components/MainBody/Slides';
+import MainNavbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <MainNavbar/>
+      <Jtron/>
+      <Slides/>
     </div>
   );
 }
---
+++ reactportfolio/src/Components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+function MainNavbar () {
+return(
+
+    <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">
+      Dev Jio
+    </Navbar.Brand>
+  </Navbar>
+
+)
+}
+
+export default MainNavbar;
